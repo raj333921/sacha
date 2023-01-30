0719ba4593d7d95eafdeb1045c5e30c22a4e7821
Rajesh Varikuntla <raj333921@gmail.com>
2023-01-30 22:11:24 +0100

    Update schema.sql
    Added insert statements for theatre

--- schema.sql
+++ schema.sql
@@ -15,8 +15,21 @@ CREATE TABLE sachadigi.theatre (
 	country VARCHAR (255) NOT NULL,
 	address VARCHAR (255) NOT NULL,
 	theatre_count INT,
-	threatre_number VARCHAR(50) NOT NULL,
+	theatre_number VARCHAR(50) NOT NULL,
 	description VARCHAR ( 255 ) NOT NULL,
 	created_on TIMESTAMP NOT NULL 
 );
 
+
+INSERT INTO sachadigi.theatre(name,city,country,address,theatre_count,theatre_number,description,created_on)
+VALUES ('White Cinemas', 'Brussels', 'Belgium', 'Edegemstraat 31', 100,'Screen 1','A nice ambience theatre',CURRENT_TIMESTAMP);
+
+INSERT INTO sachadigi.theatre(name,city,country,address,theatre_count,theatre_number,description,created_on)
+VALUES ('White Cinemas', 'Brussels', 'Belgium', 'Edegemstraat 31', 120,'Screen 2','A nice ambience theatre',CURRENT_TIMESTAMP);
+
+
+INSERT INTO sachadigi.theatre(name,city,country,address,theatre_count,theatre_number,description,created_on)
+VALUES ('White Cinemas', 'Brussels', 'Belgium', 'Edegemstraat 31', 150,'Screen 3','A nice ambience theatre',CURRENT_TIMESTAMP);
+
+INSERT INTO sachadigi.theatre(name,city,country,address,theatre_count,theatre_number,description,created_on)
+VALUES ('White Cinemas', 'Brussels', 'Belgium', 'Edegemstraat 31', 180,'Screen 4','A nice ambience theatre',CURRENT_TIMESTAMP);
