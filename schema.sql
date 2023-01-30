748f84495c0b4c29af65e68de01d0812d13d592b
Rajesh Varikuntla <raj333921@gmail.com>
2023-01-30 22:00:48 +0100

    Update schema.sql
    updated theatre schema

--- schema.sql
+++ schema.sql
@@ -7,3 +7,16 @@ CREATE TABLE sachadigi.tickets (
 	created_on TIMESTAMP NOT NULL,
   language VARCHAR (255) 
 );
+
+CREATE TABLE sachadigi.theatre (
+	theatre_id serial PRIMARY KEY,
+	name VARCHAR ( 255 ) NOT NULL,
+	city VARCHAR ( 255 ) NOT NULL,
+	country VARCHAR (255) NOT NULL,
+	address VARCHAR (255) NOT NULL,
+	theatre_count INT,
+	threatre_number VARCHAR(50) NOT NULL,
+	description VARCHAR ( 255 ) NOT NULL,
+	created_on TIMESTAMP NOT NULL 
+);
+
